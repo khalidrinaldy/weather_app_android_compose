ccd7dcdba417f8d43b24f776cf4b10bfa010065d
Khalid Rinaldy <khalid.rinaldy@nusantarainfrastructure.com>
2023-08-08 21:29:16 +0700

    add location service & weather repository

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,7 +1,9 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -33,4 +35,17 @@ fun WeatherDataDto.toWeatherDataMap() : Map<Int, List<WeatherData>> {
     } .mapValues {
         it.value.map { it.data }
     }
+}
+
+fun WeatherDto.toWeatherInfo() : WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap();
+    val now = LocalDateTime.now();
+    val currentWeather = weatherDataMap[0]?.find {
+        val hour = if(now.minute < 30) now.hour else now.hour+1
+        hour == it.time.hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeather,
+    );
 }
